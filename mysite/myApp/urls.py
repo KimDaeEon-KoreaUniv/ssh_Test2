5557bf6f6c40371869ea5705085b0cff22962478
KimDaeEon <eo956578>
2020-12-22 22:46:00 +0900

    장고 프로젝트 첫 커밋

--- mysite/myApp/urls.py
+++ mysite/myApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
-    path("",views.index, name="index")
+    path("", views.index, name="index"),
+    path("v1/", views.v1, name="v1"),
 ]
--- mysite/myApp/views.py
+++ mysite/myApp/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 def index(response):
     return HttpResponse("<h1> hiiiii </h1>")
 
+
+def v1(response):
+    return HttpResponse("v111")
+
 # Create your views here.
